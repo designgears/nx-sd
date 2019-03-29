5b2abf1a59c401ad22f47eb4eef40de26e0bf6c6
designgears <bobby@designgears.com>
2019-03-29 08:02:07 -0600

    Add lanplay module

---
+++ nxsd/components/lanplay.py
@@ -0,0 +1,60 @@
+from nxsd import util
+from nxsd.components import _dependencies as dependencies
+from nxsd.components import NXSDComponent
+from nxsd.config import settings
+from pathlib import Path
+
+LDNMITM_VERSION = 'v1.1.2'
+
+
+class LanplayComponent(NXSDComponent):
+
+    def __init__(self):
+        super().__init__()
+        self._name = 'ldn_mitm + ldnmitm_config'
+        self._version_string = LDNMITM_VERSION
+
+        self._source_directory = Path(settings.components_directory, 'lan-play/')
+
+    def has_all_dependencies(self):
+        if not dependencies.check_core_dependencies():
+            return False
+
+        return True
+
+    def install(self, install_directory):
+        self._build()
+        
+        dest_ams = Path(install_directory, 'sdcard/atmosphere/')
+        dest_nro = Path(install_directory, 'sdcard/switch/')
+
+        component_dict = {
+            'kip': (
+                Path(self._source_directory, 'ldn_mitm/ldn_mitm.kip'),
+                Path(dest_ams, 'kips/ldn_mitm.kip'),
+            ),
+            'config-app': (
+                Path(self._source_directory, 'ldnmitm_config/ldnmitm_config.nro'),
+                Path(dest_nro, 'ldnmitm_config.nro'),
+            ),
+        }
+        self._copy_components(component_dict)
+
+    def clean(self):
+        with util.change_dir(self._source_directory):
+            util.execute_shell_commands(['make clean'])
+
+    def _build(self):
+        with util.change_dir(self._source_directory):
+            build_commands = [
+                'pacman -S libnx --noconfirm',
+                'git fetch origin',
+                'git submodule update --recursive',
+                'git checkout {version}'.format(version=LDNMITM_VERSION),
+                'make',
+            ]
+            util.execute_shell_commands(build_commands)
+
+
+def get_component():
+    return LanplayComponent()
