2629d70d3504dc669e7a0222fac407e7ca32a2e9
DesignGears <bobby@designgears.com>
2019-04-13 05:02:56 -0600

    Update atmoshpere -build libnx from master -use fusee-primary.bin as main payload

--- nxsd/components/atmosphere.py
+++ nxsd/components/atmosphere.py
@@ -8,6 +8,7 @@ from pathlib import Path
 
 ATMOSPHERE_VERSION = 'v0.8.7'
 ATMOSPHERE_COMMIT_OR_TAG = 'master'
+LIBNX_COMMIT_OR_TAG = 'master'
 
 
 class AtmosphereComponent(NXSDComponent):
@@ -17,7 +18,8 @@ class AtmosphereComponent(NXSDComponent):
         self._name = 'Atmosphere'
         self._version_string = ATMOSPHERE_VERSION
 
-        self._source_directory = Path(settings.components_directory, 'atmosphere/')
+        self._atmosphere_source_directory = Path(settings.components_directory, 'atmosphere/')
+        self._libnx_source_directory = Path(settings.components_directory, 'libnx/')
 
     def has_all_dependencies(self):
         if not dependencies.check_core_dependencies():
@@ -42,60 +44,65 @@ class AtmosphereComponent(NXSDComponent):
         dest_ams = Path(install_directory, 'sdcard/atmosphere/')
         dest_sept = Path(install_directory, 'sdcard/sept/')
         dest_switch = Path(install_directory, 'sdcard/switch')
-        dest_hekate = Path(install_directory, 'sdcard/bootloader')
 
         component_dict = {
             'dmnt': (
-                Path(self._source_directory, 'stratosphere/dmnt/dmnt.nsp'),
+                Path(self._atmosphere_source_directory, 'stratosphere/dmnt/dmnt.nsp'),
                 Path(dest_ams, 'titles/010000000000000D/exefs.nsp'),
             ),
             'eclct.stub': (
-                Path(self._source_directory,
+                Path(self._atmosphere_source_directory,
                      'stratosphere/eclct.stub/eclct.stub.nsp'),
                 Path(dest_ams, 'titles/0100000000000032/exefs.nsp'),
             ),
             'fatal': (
-                Path(self._source_directory, 'stratosphere/fatal/fatal.nsp'),
+                Path(self._atmosphere_source_directory, 'stratosphere/fatal/fatal.nsp'),
                 Path(dest_ams, 'titles/0100000000000034/exefs.nsp'),
             ),
             'creport': (
-                Path(self._source_directory, 'stratosphere/creport/creport.nsp'),
+                Path(self._atmosphere_source_directory, 'stratosphere/creport/creport.nsp'),
                 Path(dest_ams, 'titles/0100000000000036/exefs.nsp'),
             ),
             'fusee-primary': (
-                Path(self._source_directory, 'fusee/fusee-primary/fusee-primary.bin'),
-                Path(dest_hekate, 'payloads/fusee-primary.bin'),
+                Path(self._atmosphere_source_directory, 'fusee/fusee-primary/fusee-primary.bin'),
+                [
+                    Path(dest_ams, 'reboot_payload.bin'),
+                    Path(install_directory, 'payload/fusee-primary.bin'),
+                ]
             ),
             'fusee-secondary': (
-                Path(self._source_directory, 'fusee/fusee-secondary/fusee-secondary.bin'),
-                Path(dest_ams, 'fusee-secondary.bin'),
+                Path(self._atmosphere_source_directory, 'fusee/fusee-secondary/fusee-secondary.bin'),
+                [
+                    Path(dest_ams, 'fusee-secondary.bin'),
+                    Path(dest_sept, 'payload.bin'),
+                ]
             ),
             'sept-primary': (
-                Path(self._source_directory, 'sept/sept-primary/sept-primary.bin'),
+                Path(self._atmosphere_source_directory, 'sept/sept-primary/sept-primary.bin'),
                 Path(dest_sept, 'sept-primary.bin'),
             ),
             'sept-secondary-enc': (
-                Path(self._source_directory, 'sept/sept-secondary/sept-secondary.enc'),
+                Path(self._atmosphere_source_directory, 'sept/sept-secondary/sept-secondary.enc'),
                 Path(dest_sept, 'sept-secondary.enc'),
             ),
             'reboot-to-payload': (
-                Path(self._source_directory, 'troposphere/reboot_to_payload/reboot_to_payload.nro'),
+                Path(self._atmosphere_source_directory, 'troposphere/reboot_to_payload/reboot_to_payload.nro'),
                 Path(dest_switch, 'reboot_to_payload.nro'),
             ),
             'hbl-html': (
-                Path(self._source_directory, 'common/defaults/hbl_html/'),
+                Path(self._atmosphere_source_directory, 'common/defaults/hbl_html/'),
                 Path(dest_ams, 'hbl_html/'),
             ),
             'no-gc': (
-                Path(self._source_directory, 'common/defaults/kip_patches/default_nogc/'),
+                Path(self._atmosphere_source_directory, 'common/defaults/kip_patches/default_nogc/'),
                 Path(dest_ams, 'kip_patches/default_nogc/'),
             ),
             'bct.ini': (
-                Path(self._source_directory, 'common/defaults/BCT.ini'),
+                Path(self._atmosphere_source_directory, 'common/defaults/BCT.ini'),
                 Path(dest_ams, 'BCT.ini'),
             ),
             'loader.ini': (
-                Path(self._source_directory, 'common/defaults/loader.ini'),
+                Path(self._atmosphere_source_directory, 'common/defaults/loader.ini'),
                 Path(dest_ams, 'loader.ini'),
             ),
             'system-settings': (
@@ -116,7 +123,7 @@ class AtmosphereComponent(NXSDComponent):
         open(Path(atmos_flags_dir, 'hbl_cal_read.flag'), 'a').close()
 
     def clean(self):
-        with util.change_dir(self._source_directory):
+        with util.change_dir(self._atmosphere_source_directory):
             util.execute_shell_commands([
                 'make clean',
             ])
@@ -124,13 +131,27 @@ class AtmosphereComponent(NXSDComponent):
     def _build(self):
         # Use a pre-built copy of sept-secondary since the keys to sign sept are not publicly available.
         os.environ['SEPT_ENC_PATH'] = str(Path(settings.defaults_directory, 'sept/sept-secondary.enc').resolve())
+        
+        self._build_libnx()
+        self._build_atmosphere()
 
-        with util.change_dir(self._source_directory):
+    def _build_atmosphere(self):
+        with util.change_dir(self._atmosphere_source_directory):
             build_commands = [
                 'git fetch origin',
                 'git submodule update --recursive',
                 'git checkout {}'.format(ATMOSPHERE_COMMIT_OR_TAG),
                 'make',
+            ]
+            util.execute_shell_commands(build_commands)
+
+    def _build_libnx(self):
+        with util.change_dir(self._libnx_source_directory):
+            build_commands = [
+                'git fetch origin',
+                'git submodule update --recursive',
+                'git checkout {}'.format(LIBNX_COMMIT_OR_TAG),
+                'make install',
             ]
             util.execute_shell_commands(build_commands)
 
