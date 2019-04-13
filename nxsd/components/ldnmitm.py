9b09e9f99f37b20c8c9a132fa22502d1cf7d288f
DesignGears <bobby@designgears.com>
2019-04-13 04:59:36 -0600

    Update ldn-mitm to new sysmodule dev branch

--- nxsd/components/ldnmitm.py
+++ nxsd/components/ldnmitm.py
@@ -5,7 +5,7 @@ from nxsd.config import settings
 from pathlib import Path
 
 LDNMITM_VERSION = 'v1.1.2'
-LDNMITM_COMMIT_OR_TAG = 'f483fab'
+LDNMITM_COMMIT_OR_TAG = 'c053868'
 
 
 class LdnmitmComponent(NXSDComponent):
@@ -15,7 +15,7 @@ class LdnmitmComponent(NXSDComponent):
         self._name = 'ldn_mitm + ldnmitm_config'
         self._version_string = LDNMITM_VERSION
 
-        self._source_directory = Path(settings.components_directory, 'ldn_mitm/')
+        self._source_directory = Path(settings.components_directory, 'ldn-mitm/')
 
     def has_all_dependencies(self):
         if not dependencies.check_core_dependencies():
@@ -30,16 +30,21 @@ class LdnmitmComponent(NXSDComponent):
         dest_nro = Path(install_directory, 'sdcard/switch/')
 
         component_dict = {
-            'kip': (
-                Path(self._source_directory, 'ldn_mitm/ldn_mitm.kip'),
-                Path(dest_ams, 'kips/ldn_mitm.kip'),
+            'ldn_mitm': (
+                Path(self._source_directory, 'ldn_mitm/out/ldn_mitm.nsp'),
+                Path(dest_ams, 'titles/4200000000000010/exefs.nsp'),
             ),
             'config-app': (
                 Path(self._source_directory, 'ldnmitm_config/ldnmitm_config.nro'),
-                Path(dest_nro, 'ldnmitm_config.nro'),
+                Path(dest_nro, 'ldnmitm_config/ldnmitm_config.nro'),
             ),
         }
         self._copy_components(component_dict)
+
+        _, ldn_mitm_stub_dir = component_dict['ldn_mitm']
+        ldn_mitm_stub_flags_dir = Path(ldn_mitm_stub_dir.parent, 'flags')
+        ldn_mitm_stub_flags_dir.mkdir(parents=True, exist_ok=True)
+        open(Path(ldn_mitm_stub_flags_dir, 'boot2.flag'), 'a').close()
 
     def clean(self):
         with util.change_dir(self._source_directory):
