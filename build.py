b765d75b67e21aa24e8d0d07adcab331ae53c16e
DesignGears <bobby@designgears.com>
2019-04-23 12:33:46 -0600

    Add single compoent building Update submodules Update .gitignore

--- build.py
+++ build.py
@@ -29,11 +29,12 @@ def main():
     }
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-v', '--verbose', action='store_true', 
-        help='enable verbose logging output to log/build.log')
-    parser.add_argument('command', nargs='?', default='build', 
-        choices=commands.keys(),
-        help='build command to execute. options: build, clean (default: build)')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='enable verbose logging output to log/build.log')
+    parser.add_argument('-c', '--component', help='build/clean a specific component')
+    parser.add_argument('command', nargs='?', default='build',
+                        choices=commands.keys(),
+                        help='build command to execute. options: build, clean (default: build)')
 
     args = parser.parse_args()
 
@@ -44,29 +45,44 @@ def main():
     commands[args.command](args)
 
 def build(args):
-    packages = get_packages()
+    if args.component != None:
+        packages = get_package(args.component)
+    else:
+        packages = get_packages()
     for package in packages:
         if package.build_components():
             nxsd.logger.info('Created {name} package!'.format(name=package.name))
         else:
             nxsd.logger.info('Failed to create {name} package! Check build.log for details.'.format(name=package.name))
-        
 
 def clean(args):
     with open('log/build.log', "w"):
+        nxsd.logger.info('Cleared log!')
         pass
-    nxsd.logger.info('Cleared log!')
-    packages = get_packages()
+    if args.component != None:
+        packages = get_package(args.component)
+    else:
+        packages = get_packages()
     for package in packages:
         package.clean()
         nxsd.logger.info('Cleaned {name} package!'.format(name=package.name))
     pass
 
+def get_package(component):
+    nxsd_component = NXSDPackage(
+        name='nxsd-{}'.format(component),
+        build_directory='build/{}/'.format(component),
+        output_filename='nxsd-{}.zip'.format(component),
+    )
+    nxsd_component.components = [eval(component)]
+
+    return [nxsd_component]
+
 def get_packages():
     nxsd_core = NXSDPackage(
         name='nxsd-core',
         build_directory='build/core/',
-        output_filename='nx-sd.zip',
+        output_filename='nxsd-core.zip',
     )
     nxsd_core.components = [
         hekate,
@@ -79,7 +95,7 @@ def get_packages():
     nxsd_addon = NXSDPackage(
         name='nxsd-addon',
         build_directory='build/addon/',
-        output_filename='nx-sd-addon.zip',
+        output_filename='nxsd-addon.zip',
     )
     nxsd_addon.components = [
         edizon,
