58eb491a698672218608ad7811c52d9ddc86981a
DesignGears <bobby@designgears.com>
2019-04-29 18:55:03 -0600

    Tidy emuiibo up a bit so it builds without errors

--- dockerfiles/emuiibo/compile.sh
+++ dockerfiles/emuiibo/compile.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# remove hardcoded directory
+sed -i 's/f:/out/g' /developer/components/emuiibo/Makefile
+
 # update libstratosphere
 cd /developer/components/emuiibo/libstratosphere
-git clean -fdx
 
 # Update Runtime Firmware Version stuff for 8.0
 git reset --hard 163d925
+
+# create build directories
+mkdir -p /developer/components/emuiibo/out/atmosphere/titles/0100000000000352/
 
 cd /
 make -j5
--- nxsd/components/emuiibo.py
+++ nxsd/components/emuiibo.py
@@ -64,8 +64,7 @@ class EmuiiboComponent(NXSDComponent):
         with util.change_dir(self._source_directory):
             build_commands = [
                 'git fetch origin',
-                'git submodule update --init --recursive',
-                'git checkout {} && git reset --hard'.format(COMPONENT_COMMIT_OR_TAG),
+                'git reset --hard {}'.format(COMPONENT_COMMIT_OR_TAG),
                 'docker run -it --rm -a stdout -a stderr --name {} --mount src="$(cd ../../ && pwd)",target=/developer,type=bind {}:latest'.format(
                     DOCKER_IMAGE_NAME, DOCKER_IMAGE_NAME),
             ]
