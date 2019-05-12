15e26b383482f35d00f7aa640ce14889e9b8de27
DesignGears <bobby@designgears.com>
2019-05-12 12:47:25 -0600

    Update hekate and amiiswap

--- nxsd/components/hekate.py
+++ nxsd/components/hekate.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 COMPONENT_NAME = 'hekate'
 COMPONENT_VERSION = 'v4.10.2J'
-COMPONENT_COMMIT_OR_TAG = '5142b77'
+COMPONENT_COMMIT_OR_TAG = '81bd4cd'
 DOCKER_IMAGE_NAME = COMPONENT_NAME.lower()+'-builder'
 
 
