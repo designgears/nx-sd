8bdb6ab5bc1ea115227ffb32602ac96f1216842f
designgears <bobby@designgears.com>
2019-07-05 14:41:52 -0600

    Update NX-Shell to 2.00

--- nxsd/components/nxshell.py
+++ nxsd/components/nxshell.py
@@ -5,8 +5,8 @@ from nxsd.config import settings
 from pathlib import Path
 
 COMPONENT_NAME = 'NX-Shell'
-COMPONENT_VERSION = 'v1.20'
-COMPONENT_COMMIT_OR_TAG = '5d0de7c'
+COMPONENT_VERSION = 'v2.00'
+COMPONENT_COMMIT_OR_TAG = 'c93c3e2'
 DOCKER_IMAGE_NAME = COMPONENT_NAME.lower()+'-builder'
 
 
