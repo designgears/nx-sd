87c6b9db5c4492e3fc8490093c68fa141eb6735c
designgears <bobby@designgears.com>
2019-08-04 18:12:49 -0600

    Updated goldleaf to v0.6

--- dockerfiles/Goldleaf/compile.sh
+++ dockerfiles/Goldleaf/compile.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-cd /developer/components/Goldleaf/libnx-Goldleaf
-git reset --hard
-git checkout master
-git reset --hard a6989b2
-
-cp /default_icon.jpg ./nx/default_icon.jpg
 
 cd /
 
