0468f26a89653a9f5bfddd6b21cdb13043f21b5e
designgears <bobby@designgears.com>
2019-08-08 11:34:35 -0600

    Updated atmosphere Updated emuiibo Cleaned up docker files

--- dockerfiles/AmiiSwap/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /developer/components/AmiiSwap
-
-make -j8
--- dockerfiles/emuiibo/compile.sh
+++ dockerfiles/emuiibo/compile.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
 # remove hardcoded directory
-sed -i 's/f:/out/g' /developer/components/emuiibo/Makefile
-
-# update libstratosphere
-cd /developer/components/emuiibo/libstratosphere
-git reset --hard 163d925 # Update Runtime Firmware Version stuff for 8.0
+sed -i 's,/mnt/f,out,g' /developer/components/emuiibo/Makefile
 
 # create build directories
 mkdir -p /developer/components/emuiibo/out/atmosphere/titles/0100000000000352/
 
-cd /
-make -j8
+make -C /developer/components/emuiibo/libstratosphere -j8
+make -C /developer/components/emuiibo -j8
