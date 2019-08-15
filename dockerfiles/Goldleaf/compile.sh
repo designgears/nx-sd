47a4e3754c9769c1df0a681d0bb52f7fac7be400
designgears <bobby@designgears.com>
2019-08-14 20:28:09 -0600

    Updated Goldleaf Simplified build script

--- dockerfiles/Goldleaf/compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-make -C /developer/components/Goldleaf/libnx install -j8
-make -C /developer/components/Goldleaf/Goldleaf -j8
--- nxsd/components/goldleaf.py
+++ nxsd/components/goldleaf.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 COMPONENT_NAME = 'Goldleaf'
 COMPONENT_VERSION = 'v0.6.1'
-COMPONENT_COMMIT_OR_TAG = '643c012'
+COMPONENT_COMMIT_OR_TAG = '7c2ce1e'
 DOCKER_IMAGE_NAME = COMPONENT_NAME.lower()+'-builder'
 
 
