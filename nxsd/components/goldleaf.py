f5a46a5876530c29d618fec51245c32b6665b255
designgears <bobby@designgears.com>
2019-08-21 09:39:07 -0600

    Update Goldleaf (0.7-dev)

--- nxsd/components/goldleaf.py
+++ nxsd/components/goldleaf.py
@@ -5,8 +5,8 @@ from nxsd.config import settings
 from pathlib import Path
 
 COMPONENT_NAME = 'Goldleaf'
-COMPONENT_VERSION = 'v0.6.1'
-COMPONENT_COMMIT_OR_TAG = 'd6772bc'
+COMPONENT_VERSION = 'v0.7-dev'
+COMPONENT_COMMIT_OR_TAG = '164ecec'
 DOCKER_IMAGE_NAME = COMPONENT_NAME.lower()+'-builder'
 
 
