4e5981432dbbfc048ba384297366c973de2b8f51
designgears <bobby@designgears.com>
2019-10-16 10:08:04 -0600

    Updated lockpick_rcm

--- nxsd/components/lockpickrcm.py
+++ nxsd/components/lockpickrcm.py
@@ -4,8 +4,8 @@ from nxsd.config import settings
 from pathlib import Path
 
 COMPONENT_NAME = 'Lockpick_RCM'
-COMPONENT_VERSION = 'v1.6.0'
-COMPONENT_COMMIT_OR_TAG = '3b6f356'
+COMPONENT_VERSION = 'v1.6.3'
+COMPONENT_COMMIT_OR_TAG = '340a256'
 DOCKER_IMAGE_NAME = COMPONENT_NAME.lower()+'-builder'
 
 class LockpickRCMComponent(NXSDComponent):
