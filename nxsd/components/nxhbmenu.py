efaf2bb7ec30aa0eac169bb4b97f8397b1848b2e
designgears <bobby@designgears.com>
2019-12-01 23:33:49 -0700

    Updated nxhbmenu

--- nxsd/components/nxhbmenu.py
+++ nxsd/components/nxhbmenu.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 COMPONENT_NAME = 'nx-hbmenu'
 COMPONENT_VERSION = 'v3.1.1'
-COMPONENT_COMMIT_OR_TAG = 'a150cbe'
+COMPONENT_COMMIT_OR_TAG = '4ecd740'
 DOCKER_IMAGE_NAME = COMPONENT_NAME.lower()+'-builder'
 
 
