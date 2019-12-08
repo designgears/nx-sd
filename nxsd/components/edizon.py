79b69eeb65aa76f311867e34709f80977da791b6
designgears <bobby@designgears.com>
2019-12-08 03:48:31 -0700

    Fixed Edizon

--- nxsd/components/edizon.py
+++ nxsd/components/edizon.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 COMPONENT_NAME = 'EdiZon'
 COMPONENT_VERSION = 'v3.1.0 - ReiNX Support'
-COMPONENT_COMMIT_OR_TAG = '5e3c997'
+COMPONENT_COMMIT_OR_TAG = 'be92abe'
 DOCKER_IMAGE_NAME = COMPONENT_NAME.lower()+'-builder'
 
 
