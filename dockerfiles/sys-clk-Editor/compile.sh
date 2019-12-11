2ddd935b3001afa1b7d23a45afea64ce1d476b8d
designgears <bobby@designgears.com>
2019-12-11 08:20:24 -0700

    Update sys-clk-editor

--- dockerfiles/sys-clk-Editor/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# apply patch
-git apply /contents.patch
-
-make -C /developer/components/sys-clk-Editor -j8
-
-# remove patch
-git apply -R /contents.patch
--- nxsd/components/sysclkeditor.py
+++ nxsd/components/sysclkeditor.py
@@ -4,8 +4,8 @@ from nxsd.config import settings
 from pathlib import Path
 
 COMPONENT_NAME = 'sys-clk-Editor'
-COMPONENT_VERSION = 'v1.2.5'
-COMPONENT_COMMIT_OR_TAG = '42d854c'
+COMPONENT_VERSION = 'v1.3.0'
+COMPONENT_COMMIT_OR_TAG = 'v1.3'
 DOCKER_IMAGE_NAME = COMPONENT_NAME.lower()+'-builder'
 
 
