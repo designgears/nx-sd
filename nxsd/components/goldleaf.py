d161a8ce822b92ed0d46cf6dc9a2e89ed8b670e9
designgears <bobby@designgears.com>
2019-12-22 00:59:58 -0700

    Updates

--- nxsd/components/goldleaf.py
+++ nxsd/components/goldleaf.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 COMPONENT_NAME = 'Goldleaf'
 COMPONENT_VERSION = 'v0.7.3'
-COMPONENT_COMMIT_OR_TAG = 'fdc9fe9'
+COMPONENT_COMMIT_OR_TAG = 'b3561ac'
 DOCKER_IMAGE_NAME = COMPONENT_NAME.lower()+'-builder'
 
 
