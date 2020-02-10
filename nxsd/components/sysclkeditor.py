8d3609edb4d801fe71009353b388d3b4958ed41b
designgears <bobby@designgears.com>
2020-02-10 11:29:39 -0700

    Update sys-clk-editor

--- nxsd/components/sysclkeditor.py
+++ nxsd/components/sysclkeditor.py
@@ -4,8 +4,8 @@ from nxsd.config import settings
 from pathlib import Path
 
 COMPONENT_NAME = 'sys-clk-Editor'
-COMPONENT_VERSION = 'v2.0'
-COMPONENT_COMMIT_OR_TAG = '536fc58'
+COMPONENT_VERSION = 'v2.0.1'
+COMPONENT_COMMIT_OR_TAG = '886a4ed'
 DOCKER_IMAGE_NAME = COMPONENT_NAME.lower()+'-builder'
 
 
