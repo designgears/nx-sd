617a9d21d8f2d09c1da8e1557617122d120e263a
designgears <bobby@designgears.com>
2020-03-02 14:39:45 -0700

    Updated Tesla-Menu

--- dockerfiles/Tesla-Menu/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /developer/components/Tesla-Menu/libs/libtesla
-
-make -j8
-
-cd /developer/components/Tesla-Menu
-
-make -j8
--- nxsd/components/teslamenu.py
+++ nxsd/components/teslamenu.py
@@ -4,8 +4,8 @@ from nxsd.config import settings
 from pathlib import Path
 
 COMPONENT_NAME = 'Tesla-Menu'
-COMPONENT_VERSION = 'v1.0.1'
-COMPONENT_COMMIT_OR_TAG = 'c6ed6e0'
+COMPONENT_VERSION = 'v1.0.2'
+COMPONENT_COMMIT_OR_TAG = '6e16505'
 DOCKER_IMAGE_NAME = COMPONENT_NAME.lower()+'-builder'
 
 
